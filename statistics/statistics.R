de3dd61bf446653e7db1c6914ed8d21d2b1ce833
jstawarz <89322009+jstawarz@users.noreply.github.com>
2022-02-01 10:52:50 -0500

    Add files via upload

---
+++ statistics/statistics.R
@@ -0,0 +1,32 @@
+#Add library
+library(dplyr)
+
+#import csv
+weather <- read.csv('weather_cleaned.csv',stringsAsFactors = FALSE)
+
+str(weather)
+
+#calculate correlation matrix
+used_matrix <- as.matrix(weather[,c("visit","Average.Snow.Depth","Average.High.Temp","Average.Low.Temp","Average.Temp")])
+cor(used_matrix)
+
+#linear regression for snow depth
+lm(visit ~ Average.Snow.Depth, weather)
+
+#summarize linear regression for snow depth
+summary(lm(visit ~ Average.Snow.Depth, weather))
+
+#linear regression for average temp
+lm(visit ~ Average.Temp, weather)
+
+#summarize linear regression for average temp
+summary(lm(visit ~ Average.Temp, weather))
+
+#import csv
+reviews <- read.csv('reviews_cleaned.csv',stringsAsFactors = FALSE)
+
+str(reviews)
+
+#calculate correlation matrix
+review_matrix <- as.matrix(reviews[,c("rating","Average.Snow.Depth","Average.High.Temp","Average.Low.Temp","Average.Temp")])
+cor(review_matrix)
