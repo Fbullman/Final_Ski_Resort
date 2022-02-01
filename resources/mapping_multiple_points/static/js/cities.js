e7a03d06271f3b5b98a808202885f0013fecbc7c
jstawarz <89322009+jstawarz@users.noreply.github.com>
2022-01-31 20:49:36 -0500

    Add files via upload

---
+++ resources/mapping_multiple_points/static/js/cities.js
@@ -0,0 +1,74 @@
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [45.818, -110.897],
+    city: "Bridger",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [45.81667, -110.88333],
+    city: "Bridger Weather",
+    state: "MT",
+    population: 500 
+  },
+  {
+    location: [46.256389, -113.241944],
+    city: "Discovery",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [46.24, -113.77],
+    city: "Discovery Weather",
+    state: "MT",
+    population: 500
+  },
+  {
+    location: [45.191, -109.335],
+    city: "Red Lodge",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [45.19, -109.35],
+    city: "Red Lodge Weather",
+    state: "MT",
+    population: 500 
+  },
+  {
+    location: [46.79, -110.68],
+    city: "Showdown Weather",
+    state: "MT",
+    population: 500 
+  },
+  {
+    location: [46.838611, -110.708889],
+    city: "Showdown",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [47.02, -114],
+    city: "Snowbowl",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [46.98, -115.03],
+    city: "Snowbowl Weather",
+    state: "MT",
+    population: 500
+  },
+  {
+    location: [48.5, -114.34],
+    city: "Whitefish",
+    state: "MT",
+    population: 10000
+  },
+  {
+    location: [48.5497, -114.5739],
+    city: "Whitefish Weather",
+    state: "MT",
+    population: 500 
+  }
+  ];
---
+++ resources/mapping_multiple_points/static/js/config.js
@@ -0,0 +1,2 @@
+// API Key
+const API_KEY = "pk.eyJ1IjoianN0YXdhcnoiLCJhIjoiY2t3MnExYWh1MGRqbzJubXBrMXNibGFiZSJ9.NGrBgcWxX3MBt6fcIyHIYw";
